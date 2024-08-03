5e1f2e69b011f8278ac584bebe0fcc4b73a53365
ErdalMutlu <102668295+Erdal-Mutlu@users.noreply.github.com>
2024-08-03 20:35:30 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, Education World!'
+    return 'Hello, Education World! - Version 0001'
 
 @app.route('/about')
 def about():
     return 'This is the About page.'
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=80)
